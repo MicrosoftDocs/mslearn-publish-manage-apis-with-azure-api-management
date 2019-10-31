2659ff13d149824f033f84d56b9306c15485a36f
Nick Walker <nickwalk@microsoft.com>
2019-10-31 10:49:37 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -21,7 +21,7 @@ PLAN_NAME=myPlan
 printf "\nCreating App Service plan in FREE tier ... (2/7)\n\n"
 
 
-az appservice plan create --name $apiappname --resource-group $RESOURCE_GROUP --sku FREE --verbose
+az appservice plan create --name $apiappname --resource-group $RESOURCE_GROUP --sku FREE --location centralus --verbose
 
 printf "\nCreating API App ... (3/7)\n\n"
 
