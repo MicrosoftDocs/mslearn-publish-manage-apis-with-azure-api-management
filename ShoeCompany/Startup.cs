6d3fb331c944423fd7244a091bebcc0a2e525d43
sundara_rajan <Sundara_Rajan@epam.com>
2024-02-17 12:08:47 +0000

    updated to .Net version 7

--- ShoeCompany/Startup.cs
+++ ShoeCompany/Startup.cs
@@ -26,7 +26,7 @@ namespace ShoeCompany
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
 
             // Configure Swagger
             services.AddSwaggerGen(c =>
